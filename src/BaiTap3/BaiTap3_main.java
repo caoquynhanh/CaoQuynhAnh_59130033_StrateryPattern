3c799de6a6099f363e5aa9e11d1f5c76f3199014
Cao Quỳnh Anh <quynhanhcao99@gmail.com>
2020-04-23 11:06:56 +0700

    fix

--- src/BaiTap3/BaiTap3_main.java
+++ src/BaiTap3/BaiTap3_main.java
@@ -4,14 +4,45 @@
  * and open the template in the editor.
  */
 package BaiTap3;
-
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.ArrayList;
+import java.util.Collections;
+import BaiTap3.QLSV;
+import java.text.ParseException;
 /**
  *
  * @author Asus
  */
 public class BaiTap3_main 
 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException
+    {
+        QLSV qldsSV = new QLSV();
+        SinhVien sv1 = new SinhVien("Tran Ba Duy",new SimpleDateFormat("dd/MM/yy").parse("20/01/1991"),8);
+        SinhVien sv2 = new SinhVien("Chung Thanh Phong",new SimpleDateFormat("dd/MM/yy").parse("21/02/1987"),7);
+        SinhVien sv3 = new SinhVien("Ly Quy Khanh",new SimpleDateFormat("dd/MM/yy").parse("22/03/1988"),6);
+        SinhVien sv4 = new SinhVien("Le Thao Nhi",new SimpleDateFormat("dd/MM/yy").parse("23/04/1995"),9);
         
+        qldsSV.themSV(sv1);
+        qldsSV.themSV(sv2);
+        qldsSV.themSV(sv3);
+        qldsSV.themSV(sv4);
+        
+        System.out.print("Danh sach sinh vien khoi tao: ");
+        qldsSV.inDS();
+        
+        ISoSanh ssSinhVienTheoDiem = new SoSanhTheoDiem();
+        ISoSanh ssSinhVienTheoTen = new SoSanhTheoTen();
+        
+        qldsSV.setSoSanh(ssSinhVienTheoDiem);
+        qldsSV.sapXep();
+        System.out.println("Danh sach sinh vien sap xep theo ten: ");
+        qldsSV.inDS();
+        
+        qldsSV.setSoSanh(ssSinhVienTheoTen);
+        qldsSV.sapXep();
+        System.out.println("Danh sach sinh vien sap xep theo diem: ");
+        qldsSV.inDS();
     }
 }
